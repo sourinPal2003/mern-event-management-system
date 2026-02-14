a36a956114a270c932d3e8d12952361e4c642601
sourinPal2003 <sourinpal2003@gmailcom>
2026-02-14 11:28:16 +0530

    for any path if user not found nevigate to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,7 @@ function AppRoutes() {
         <>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </>
       ) : (
         <>
